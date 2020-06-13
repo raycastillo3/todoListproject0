0dba34669f1c00b533ee7c3de79ad4a5be22cc60
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2020-06-12 21:28:24 -0400

    message ("This is project0")

---
+++ script.js
@@ -0,0 +1,33 @@
+
+  const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+} 
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+  
+
+function newTodo() {
+var Item = document.getElementById("item-count")
+var newItem = document.createElement('li')
+newItem.appendChild(Item)
+
+
+//document.getElementById("todo-list").appendChild(newItem)
+// document.getElementById("newTodo").appendChild(newItem)
+
+  //alert('New TODO button clicked!')
+}
+
+
+
+
+
+//console.log (typeof newTodo)
+
+
+
+//Your goal will be to get this button to create new TODOs.
