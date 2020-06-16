e794e7095340dcf68d6f9851030b08248c695a7e
Raymond Castillo <60750068+raycastillo3@users.noreply.github.com>
2020-06-15 23:01:27 -0400

    working in the challenge

--- script.js
+++ script.js
@@ -9,12 +9,24 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
   
-var i = 0
+var i = 0;
 function newTodo() {
-  var item = parseInt(document.getElementById("item-count").item = ++i);
+  var item = parseInt(document.getElementById("item-count")) //.item = ++i);
+  item = ++i
   item = isNaN(item) ? 0: item;
   document.getElementById ('item-count').innerHTML = item;
-  alert('New TODO button clicked!');
+  //alert('New TODO button clicked!');
 }
 
+
+function deleteTodo(){
+ var deleteItem = parseInt(document.getElementById("unchecked-count").deleteItem);
+ deleteItem = isNaN(deleteItem) ? 0: deleteItem;
+ document.getElementById ('unchecked-count').innerHTML = deleteItem;
+ document.getElementById('unchecked-count').appendChild('item-count') - 1; 
+ alert ('a todo has been deleted');
+
+
+
+}
 
