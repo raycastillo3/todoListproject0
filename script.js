f4bb27b44023be21ffcea7c86966210bd0f8c490
Raymond Castillo <60750068+raycastillo3@users.noreply.github.com>
2020-06-17 06:10:55 -0400

    couldn't figure out the challenge

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
   
 var i = 0;
-function newTodo() {
+function newTodo(item) {
   var item = parseInt(document.getElementById("item-count")) //.item = ++i);
   item = ++i
   item = isNaN(item) ? 0: item;
   document.getElementById ('item-count').innerHTML = item;
-  //alert('New TODO button clicked!');
+  alert('New TODO button clicked!');
 }
 
 
 function deleteTodo(){
- var deleteItem = parseInt(document.getElementById("unchecked-count").deleteItem);
+ var deleteItem = parseInt(document.getElementById("unchecked-count"));
+ deleteItem = --i;
  deleteItem = isNaN(deleteItem) ? 0: deleteItem;
  document.getElementById ('unchecked-count').innerHTML = deleteItem;
- document.getElementById('unchecked-count').appendChild('item-count') - 1; 
- alert ('a todo has been deleted');
+ alert ('A todo has been deleted!');
 
 
 
